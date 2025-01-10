12cac1edcd72ba26649a03a9826329019f6f910e
Niklas Wahl <n.wahl@dkfz-heidelberg.de>
2025-01-10 13:33:55 +0100

    remove old GUI file
    Signed-off-by: Niklas Wahl <n.wahl@dkfz-heidelberg.de>

--- pyRadPlan/GUI.py
+++
@@ -1,5 +0,0 @@
-import pyRadPlan.matRad._engine as eng
-
-
-def matRadGUI(ct, cst, pln, resultGUI):
-    eng.currentEngine.engine.callFromPython("matRadGUI", "GUI", ct, cst, pln, resultGUI, nargout=0)
