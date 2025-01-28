867eec14e89c4b4d8048b24ec9f66c6aedfdcd71
Niklas Wahl <n.wahl@dkfz-heidelberg.de>
2025-01-28 22:26:15 +0000

    implement kernel convolution per beam in photon engine

--- pyRadPlan/dose/engines/_svdpb.py
+++ pyRadPlan/dose/engines/_svdpb.py
@@ -3,14 +3,19 @@ Implementation of a pencil beam dose calculation engine for photon beams
 based on the Singular-value
 decomposition (SVD) method by Bortfeld.
 """
-from typing import TypedDict, Literal, Any
+from typing import TypedDict, Literal, Any, cast
 import logging
 import random
 
 import numpy as np
+import scipy.fft as fft
 
 from pyRadPlan.plan import PhotonPlan
+
+# from pyRadPlan.stf import Beam
+from pyRadPlan.machines import PhotonLINAC
 from ._base_pencilbeam import PencilBeamEngineAbstract
+
 
 logger = logging.getLogger(__name__)
 
@@ -61,7 +66,7 @@ class PhotonPencilBeamSVDEngine(PencilBeamEngineAbstract):
     enable_dij_sampling: bool = True
     dij_sampling: DijSamplingConfig
 
-    def __init__(self, pln: PhotonPlan):  # more parameters necessary for both classes. !
+    def __init__(self, pln: PhotonPlan):
 
         self.use_custom_primary_photon_fluence = False
         self.kernel_cutoff = 10.0
@@ -72,7 +77,7 @@ class PhotonPencilBeamSVDEngine(PencilBeamEngineAbstract):
             rel_dose_threshold=0.01, lat_cut_off=20, type="radius", delta_rad_depth=5
         )
 
-        super().__init__(PhotonPlan)
+        super().__init__(pln)
 
         # Protected/Private attributes (equivalent to SetAccess = protected)
         self._is_field_based_dose_calc = None  # will be set
@@ -118,30 +123,9 @@ class PhotonPencilBeamSVDEngine(PencilBeamEngineAbstract):
                 "Kernel cutoff smaller than the geometric lateral cutoff. Using geometric cutoff."
             )
 
-        if self._is_field_based_dose_calc:
-            logger.debug("Enabling field-based dose calculation!")
-            self.int_conv_resolution = self._collimation["conv_resolution"]
-            self._field_width = self._collimation["field_width"]
-        else:
-            logger.debug("Enabling bixel-based dose calculation!")
-
-            bws = np.unique(np.asarray([b.bixel_width for b in stf.beams], dtype=float))
-
-            if len(bws) != 1:
-                raise ValueError(
-                    "Bixel widths inconsistent across beams, which is currently not supported!"
-                )
-
-            self._field_width = bws[0]
-
-        field_limit = np.ceil(self._field_width / (2 * self.int_conv_resolution))
-        # TODO: should this be +1 as end or is it correct? In matRad it is equivalent to no +1
-        field_grid = self.int_conv_resolution * np.arange(-field_limit, field_limit)
-        self._f_x, self._f_z = np.meshgrid(field_grid, field_grid, indexing="xy")
-
         # matRad does the gaussian filter here, but can we do that here?
         # We should be as flexible as to allow beams with different energies / penumbras / kernels
-        # moved the krnel filtering to beam initialization
+        # moved the kernel filtering to beam initialization
 
         # Initialize random number generator
         random.seed(self.random_seed)
@@ -169,6 +153,83 @@ class PhotonPencilBeamSVDEngine(PencilBeamEngineAbstract):
 
         beam_info = super()._init_beam(beam_info, ct, cst, stf, i)
 
+        field_based_dose_calc = False
+        if self._is_field_based_dose_calc:
+            logger.debug("Enabling field-based dose calculation for beam %d!", i)
+            self.int_conv_resolution = self._collimation["conv_resolution"]
+            field_width = self._collimation["field_width"]
+            field_based_dose_calc = True
+        else:
+            logger.debug("Enabling bixel-based dose calculation for beam %d!", i)
+            field_width = beam_info["beam"]["bixel_width"]
+
+        # TODO: obtain maximum field limits
+
+        field_limit = np.ceil(field_width / (2 * self.int_conv_resolution))
+        # TODO: should this be +1 as end or is it correct? In matRad it is equivalent to no +1
+        field_grid = self.int_conv_resolution * np.arange(-field_limit, field_limit)
+        beam_info["f_x"], beam_info["f_z"] = np.meshgrid(field_grid, field_grid, indexing="xy")
+
+        # Get the kernel
+        beamlets = [beamlet for ray in beam_info["beam"]["rays"] for beamlet in ray["beamlets"]]
+
+        energies = np.unique([beamlet["energy"] for beamlet in beamlets])
+
+        if len(energies) > 1:
+            raise ValueError("Different energies in one photon beam not supported yet.")
+
+        energy = energies[0]
+
+        kernel = cast(PhotonLINAC, self._machine).get_kernel_by_energy(energy)
+
+        sigma_gauss = kernel.penumbra / np.sqrt(8 * np.log(2))  # [mm]
+
+        # use 5 times sigma as the limits for the gaussian convolution
+        gauss_limit = np.ceil(5 * sigma_gauss / self.int_conv_resolution)
+        gauss_grid = self.int_conv_resolution * np.arange(-gauss_limit, gauss_limit)
+
+        gauss_filter_x, gauss_filter_z = np.meshgrid(gauss_grid, gauss_grid, indexing="xy")
+        gauss_filter = (
+            1.0
+            / (2 * np.pi * sigma_gauss**2 / self.int_conv_resolution**2)
+            * np.exp(-(gauss_filter_x**2 + gauss_filter_z**2) / (2 * sigma_gauss**2))
+        )
+
+        gauss_conv_size = 2 * (field_limit + gauss_limit).astype(int)
+
+        # get kernel size and distances
+        kernel_limit = np.ceil(self.kernel_cutoff / self.int_conv_resolution)
+        kernel_grid = self.int_conv_resolution * np.arange(-kernel_limit, kernel_limit)
+
+        kernel_x, kernel_z = np.meshgrid(kernel_grid, kernel_grid, indexing="xy")
+
+        # calculate also the total size and distance as we need this during convolution extensively
+        kernel_conv_limit = field_limit + gauss_limit + kernel_limit
+        kernel_conv_grid = self.int_conv_resolution * np.arange(
+            -kernel_conv_limit, kernel_conv_limit
+        )
+        conv_mx_x, conv_mx_z = np.meshgrid(kernel_conv_grid, kernel_conv_grid, indexing="xy")
+
+        kernel_conv_size = 2 * kernel_conv_limit.astype(int)
+
+        effective_lateral_cut_off = self.geometric_lateral_cutoff + field_width / np.sqrt(2)
+
+        if not field_based_dose_calc:
+            n = np.floor(field_width / self.int_conv_resolution).astype(int)
+            f_pre = np.ones((n, n), dtype=np.float32)
+
+            if not self.use_custom_primary_photon_fluence:
+                f_pre = fft.ifft2(
+                    fft.fft2(f_pre, (gauss_conv_size, gauss_conv_size))
+                    * fft.fft2(gauss_filter, (gauss_conv_size, gauss_conv_size))
+                )
+                f_pre = np.real(f_pre)
+
+        beam_info["f_pre"] = f_pre
+        beam_info["kernel_xz"] = (kernel_x, kernel_z)
+        beam_info["conv_mx_xz"] = (conv_mx_x, conv_mx_z)
+
+        raise NotImplementedError("You shall not pass!")
         # get index of central ray or closest to the central ray
         # center = np.argmin(
         # np.sum(np.array([ray["ray_pos_bev"] for ray in beam_info["beam"]["rays"]]) ** 2, axis=1)
@@ -182,10 +243,7 @@ class PhotonPencilBeamSVDEngine(PencilBeamEngineAbstract):
         # Display console message
         # logger.info(f"\tSSD = {self.machine.data.kernel[curr_ssd_ix].SSD} mm ...")
 
-        raise NotImplementedError("This method is not implemented yet.")
-
-        currBeam = []
-        return currBeam
+        return beam_info
 
     def _compute_bixel(self, curr_ray, k):
         """
